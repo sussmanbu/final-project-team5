2ddeb788c718b89365f45819913157381933a0c5
Peizi Wang <pzwang@bu.edu>
2024-04-29 21:43:00 -0400

    Built site for gh-pages

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -16,8 +16,25 @@ compas_data$Age <- as.numeric(difftime(compas_data$Screening_Date, compas_data$D
 compas_data_clean <- compas_data %>%
   select(Person_ID, Sex = Sex_Code_Text, Race = Ethnic_Code_Text, DateOfBirth, Age, RawScore, DecileScore, ScoreText) %>% 
   drop_na()
-  
-write_csv(compas_data_clean, file = here::here("dataset", "compas-scores-clean.csv"))
 
-save(compas_data_clean, file = here::here("dataset/compas-scores-clean.RData"))
+# second dataset
+iowa_data <- read_csv(here::here("dataset", "Iowa_Probation_Recidivism_Status_20240403.csv"))
+iowa_data_clean <- iowa_data %>%
+  select(sex = Sex, race = Race, age = Age, reincarcerated = Reincarcerated)
+
+# 3rd dataset (new compas)
+new_compas <- read_csv(here::here("dataset", "compas-scores-two-years-violent.csv"))
+new_compas_clean <- new_compas %>%
+  select(sex, age, race, reincarcerated)
+
+## COMBINING the data
+new_compas_clean <- new_compas_clean %>% mutate(state = "Florida")
+iowa_data_clean <- iowa_data_clean %>% mutate(state = "Iowa")
+combined_data <- bind_rows(new_compas_clean, iowa_data_clean)
+write_csv(combined_data, file = here::here("dataset", "combined-data.csv"))
+
+## SAVING the data  
+write_csv(compas_data_clean, file = here::here("dataset", "compas-scores-clean.csv"))
+write_csv(iowa_data_clean, file = here::here("dataset", "iowa-data-clean.csv"))
+write_csv(new_compas_clean, file = here::here("dataset", "new-compas-clean.csv"))
 
